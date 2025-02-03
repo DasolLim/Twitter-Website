13e5a244fc107591c19e0d6699dc67aaeeb34f3f
Dasol Lim <dlim67@uwo.ca>
2025-02-02 21:39:42 -0500

    Add provider section and update login page layout

--- twitter/pages/api/login.js
+++ twitter/pages/api/login.js
@@ -1,5 +1,18 @@
-export default function LoginPage() {
+export default function LoginPage(providers) {
     return (
-        <div>login page here</div>
+        <div className="flex items-center justify-center h-screen">
+            {Object.values(providers).map(provider => (
+                <div>
+                    <button className="">Sign in with {provider.name}</button>
+                </div>
+            ))}
+        </div>
     );
+}
+
+export async function getServerSideProps() {
+    const providers = await getProviders();
+    return {
+        props: { providers },
+    }
 }
